6de543bfa23c559abeb9b5049495e0a436841fc8
ecc311 <greewolf_3@hotmail.com>
2014-02-24 18:04:28 +0100

    Implementada funcionalitat Ajax

--- struts2exemple/WebContent/Menu.jsp
+++ struts2exemple/WebContent/Menu.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <s:a href="customer-form">Afegir Client</s:a><br>
-<s:a href="userImage">Carregar Imatge</s:a>
+<s:a href="userImage">Carregar Imatge</s:a><br>
+<s:a href="ajaxdemo">Carregar Client</s:a>
---
+++ struts2exemple/src/src/paquet/AjaxAutocomplete.java
@@ -0,0 +1,118 @@
+package src.paquet;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import java.sql.Statement;
+ 
+
+import com.opensymphony.xwork2.ActionSupport;
+ 
+@SuppressWarnings("serial")
+public class AjaxAutocomplete extends ActionSupport {
+    
+	private static Connection conn;
+	private String sql;
+	private String data;
+    private List<String> countries;
+    private String country;
+     
+    public String execute() {
+    	String datas="";
+    	ArrayList<String> noms=new ArrayList<String>();
+    	noms=carregardata();
+    	for( int i = 0 ; i < noms.size() ; i++ ){
+    		  datas=( noms.get( i )+"," );
+    	}
+    	data = datas.substring(0, datas.length()-1); 
+        countries = new ArrayList<String>();
+        StringTokenizer st = new StringTokenizer(data, ",");
+ 
+        while (st.hasMoreTokens()) {
+            countries.add(st.nextToken().trim());
+        }
+        return SUCCESS;
+    }
+    public ArrayList<String> carregardata()
+    {
+    	init();
+    	
+    	
+    	ArrayList<String> llista=new ArrayList<String>();
+    	
+    	try {
+    			Statement stmt = conn.createStatement();
+    			sql = "SELECT nom FROM client";
+    			ResultSet aux = stmt.executeQuery(sql);
+    			while(aux.next())
+    			{
+    			String ida = aux.getString("nom");
+    			llista.add(ida);
+    			
+    			}
+    			for(int i=0;i<llista.size();i++)
+    			{
+    				
+    				System.out.println(llista.get(i));
+    			}
+    			conn.close();
+    		} catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+
+    		return llista;
+    	}
+    	
+    	
+    
+    
+    public void init() {
+
+		conn=null;
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String servidor="jdbc:mysql://localhost/client";
+			String usuarioDB="root";
+			String passwordDB="";
+			conn=DriverManager.getConnection(servidor,usuarioDB,passwordDB);
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			conn=null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			conn=null;
+		}catch (Exception e){
+			
+			e.printStackTrace();
+			conn=null;
+		}
+	}
+    
+    public String getCountry() {
+        return this.country;
+    }
+ 
+    public List<String> getCountries() {
+        return countries;
+    }
+ 
+    public void setCountries(List<String> countries) {
+        this.countries = countries;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
